aa64bb1096d2441e10b9f6ce223529fe943a4227
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-09-09 10:20:34 +0530

    Updates

---
+++ assets/Home-CcVWIbug.js
@@ -0,0 +1,19 @@
+import{j as r,T as i,d as s}from"./index-k4R5R2SR.js";function t(){return r.jsx(r.Fragment,{children:r.jsxs(d.Wrapper,{children:[r.jsx("h1",{children:r.jsx(i,{keyName:"home.title"})}),r.jsx("p",{children:r.jsx(i,{keyName:"home.sub"})}),r.jsxs(d.Card,{children:[r.jsx("strong",{children:"Why this app?"}),r.jsxs("ul",{children:[r.jsx("li",{children:"Real-world architecture (routing, theming, i18n, state)."}),r.jsx("li",{children:"Production-ready UI with styled-components."}),r.jsx("li",{children:"Accessibility-first forms & validations."}),r.jsxs("li",{children:["Designed and developed by ",r.jsx("a",{href:"https://www.ashishranjan.net",target:"_blank",children:"https://www.ashishranjan.net"})]})]})]})]})})}const d={Wrapper:s.div`
+        /* border: 1px solid #f00; */
+    `,Card:s.div`
+        background: ${({theme:e})=>e.card};
+        border: 1px solid ${({theme:e})=>e.border};
+        border-radius: 16px;
+        padding: 20px;
+        margin-top: 15px;
+
+        ul {
+            margin-left: 30px;
+            li {
+                a {
+                    padding: 3px;
+                    border-bottom: 1px solid #fff;;
+                }
+            }
+        }
+    `};export{t as default};
